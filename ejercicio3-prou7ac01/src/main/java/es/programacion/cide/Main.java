6672ce0c9eff34d1f656a0a1e1bb770ca1675d88
fran <frodriguez4@nigul.cide.es>
2026-03-17 18:50:58 +0100

    finished file activity 3

---
+++ ejercicio3-prou7ac01/src/main/java/es/programacion/cide/Main.java
@@ -0,0 +1,31 @@
+package es.programacion.cide;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String texto;
+        FileOutputStream archivo;
+
+        archivo= null;
+
+        try {
+            texto = "Hola, estic aprenent Java!";
+            archivo = new FileOutputStream("datos.txt");
+            for (char c : texto.toCharArray()) { // Eso hace que el for recorra cada caracter del texto
+                archivo.write(c); //esto escribre el caracter en el archivo datos.txt
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (archivo != null) {
+                try {
+                    archivo.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
