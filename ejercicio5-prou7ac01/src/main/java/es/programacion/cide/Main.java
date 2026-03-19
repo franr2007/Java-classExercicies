0169ef7dc35e1498817d4afbc47360acc200276d
fran <frodriguez4@nigul.cide.es>
2026-03-19 18:18:52 +0100

    finished file last activity 5

--- ejercicio5-prou7ac01/src/main/java/es/programacion/cide/Main.java
+++ ejercicio5-prou7ac01/src/main/java/es/programacion/cide/Main.java
@@ -1,7 +1,48 @@
 package es.programacion.cide;
 
+import java.io.File;
+import java.util.Scanner;
+
+//41663903R
+//francisco javier rodriguez magaña
+//19-03-26
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc;
+        String ruta;
+        File archivo;
+
+        sc=new Scanner(System.in);
+        System.out.println("pon una ruta");
+        ruta= sc.nextLine();
+        archivo= new File(ruta);
+
+        if (!archivo.exists()) {
+            System.out.println("La ruta no existe");
+        }
+        else{
+            if (archivo.isDirectory()) {
+                System.out.println("Es un directorio");
+            }
+            else{
+                System.out.println("Es un archivo");
+            }
+            if (archivo.canRead()) {
+                System.out.println("Tienes permisos de lectura");
+            }
+            else{
+                System.out.println("No tienes permisos de lectura");
+            }
+            if (archivo.canWrite()) {
+                System.out.println("Tienes permisos de escritura");
+            }
+            else{
+                System.out.println("No tienes permisos de escritura");
+            }
+            System.out.println(archivo.getAbsolutePath());
+        }
+
+        sc.close();
     }
 }
