23c83bae5880356ecbbd938e1289aa218ba88b6b
fran <frodriguez4@nigul.cide.es>
2026-03-23 18:22:41 +0100

    added a new method, can open win explorer & select a file

--- Entregables/archivos-prou7ex02/src/main/java/es/programacion/cide/EditorTxt.java
+++ Entregables/archivos-prou7ex02/src/main/java/es/programacion/cide/EditorTxt.java
@@ -1,15 +1,21 @@
 package es.programacion.cide;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.TextArea;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
-public class Botones extends JPanel {
+public class EditorTxt extends JPanel {
     // atributos
     private GridBagConstraints g;
     private JButton botonCrear;
@@ -19,10 +25,15 @@ public class Botones extends JPanel {
     private int altoBtn;
     private int anchobtn;
     private Font fuente;
+    private TextArea area;
+    private JPanel panelBotones;
 
     // constructor
-    public Botones() {
-        setLayout(new GridBagLayout());
+    public EditorTxt() {
+        setLayout(new BorderLayout());
+
+        panelBotones = new JPanel();
+        panelBotones.setLayout(new GridBagLayout());
 
         altoBtn = 50;
         anchobtn = 180;
@@ -32,9 +43,11 @@ public class Botones extends JPanel {
 
         botonCrear = new JButton("NUEVO 📄");
         botonCrear.setPreferredSize(new Dimension(anchobtn, altoBtn));
+        botonCrear.addActionListener(e -> nuevo());
 
         botonAbrir = new JButton("ABRIR 📂");
         botonAbrir.setPreferredSize(new Dimension(anchobtn, altoBtn));
+        botonAbrir.addActionListener(e -> abrir());
 
         botonGuardar = new JButton("GUARDAR 💾");
         botonGuardar.setPreferredSize(new Dimension(anchobtn, altoBtn));
@@ -42,17 +55,22 @@ public class Botones extends JPanel {
         botonGuardarComo = new JButton("GUARDAR COMO 💾+");
         botonGuardarComo.setPreferredSize(new Dimension(anchobtn, altoBtn));
 
+        area = new TextArea();
+
         definirEstilos(fuente);
 
         g.gridx = 0;
         g.gridy = 0;
-        add(botonCrear, g);
+        panelBotones.add(botonCrear, g);
         g.gridx = 1;
-        add(botonAbrir, g);
+        panelBotones.add(botonAbrir, g);
         g.gridx = 2;
-        add(botonGuardar, g);
+        panelBotones.add(botonGuardar, g);
         g.gridx = 3;
-        add(botonGuardarComo, g);
+        panelBotones.add(botonGuardarComo, g);
+
+        add(panelBotones, BorderLayout.NORTH);
+        add(area, BorderLayout.CENTER);
     }
 
     // getters y setters
@@ -75,5 +93,34 @@ public class Botones extends JPanel {
         botonGuardar.setBackground(new Color(210, 225, 245));
         botonGuardarComo.setForeground(new Color(58, 110, 165));
         botonGuardarComo.setBackground(new Color(210, 225, 245));
+        panelBotones.setBackground(new Color(210, 225, 245));
+        area.setBackground(new Color(240, 245, 252));
+        area.setForeground(new Color(26, 46, 80));
+        area.setFont(new Font("MonoSpace", 0, 18));
     }
+
+    public void nuevo() {
+        area.setText("");
+    }
+
+    public void abrir() {
+        JFileChooser selector = new JFileChooser(); //abre el explorador de archivos
+        if (selector.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) { //mira si se ha elegido algun archivo
+            try {
+                String contenido = Files.readString(selector.getSelectedFile().toPath());//esto coje el contenido del texto
+                area.setText(contenido);//mete el contenido anterior en el area
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void guardar() {
+
+    }
+
+    public void guardarComo() {
+
+    }
+
 }
--- Entregables/archivos-prou7ex02/src/main/java/es/programacion/cide/Main.java
+++ Entregables/archivos-prou7ex02/src/main/java/es/programacion/cide/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         // atributos
         JFrame ventana;
-        Botones panelBotones;
-        TextArea editorTxt;
+        EditorTxt panelBotones;
+        
 
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
@@ -24,16 +24,10 @@ public class Main {
         ventana.setResizable(false);
         ;
 
-        panelBotones = new Botones();
+        panelBotones = new EditorTxt();
         panelBotones.setBackground(new Color(180, 205, 235));
 
-        editorTxt = new TextArea();
-        editorTxt.setBackground(new Color(240, 245, 252));
-        editorTxt.setForeground(new Color(26, 46, 80));
-        editorTxt.setFont(new Font("MonoSpace", 0, 18));
-
-        ventana.add(panelBotones, BorderLayout.NORTH);
-        ventana.add(editorTxt, BorderLayout.CENTER);
+        ventana.add(panelBotones, BorderLayout.CENTER);
 
         ventana.setVisible(true);
     }
