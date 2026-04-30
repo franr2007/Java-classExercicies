d203a356d63b751f81a3a52114da4b075e1234eb
fran <frodriguez4@nigul.cide.es>
2026-04-30 18:51:57 +0200

    still workin on nominaDAO

--- Entregables/prou8ex01-fran/src/main/java/es/programacion/cide/DAO/EmpleadoDao.java
+++ Entregables/prou8ex01-fran/src/main/java/es/programacion/cide/DAO/EmpleadoDao.java
@@ -10,28 +10,37 @@ public class EmpleadoDao implements Dao<Empleado, Integer> {
 
     @Override
     public void insertar(Empleado empleado) {
+        // este es el comando en sqlite para insertar valores a la base de datos
         String comando = "INSERT INTO Empleado (NSS,NOM,APELLIDOS,EMAIL,IBAN) VALUES(?,?,?,?,?)";
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando
         try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando)) {
+            // por cada interrogante dentro de values recojemos los datos del empleado y se
+            // pone en values
             statement.setInt(1, empleado.getNss());
             statement.setString(2, empleado.getNom());
             statement.setString(3, empleado.getApellidos());
             statement.setString(4, empleado.getEmail());
             statement.setString(5, empleado.getIban());
         } catch (SQLException e) {
+            // en caso de error al preparar la conexion, se imprime el mensaje del error
             System.err.println("Err insert empelado: " + e.getMessage());
         }
     }
 
     @Override
     public Empleado recojerPorId(Integer id) {
+        // este es el comando que servira para recojer a un empleado por id
         String comando = "SELECT * FROM EMPLEADO WHERE ID=?";
-
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
         try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
                 ResultSet resultado = statement.executeQuery()) {
+            // se le pasa el valor del interogante, con el id que se busca
             statement.setInt(1, id);
 
+            // este bucle lista a los empleados que contenga el numero de id
             while (resultado.next()) {
-                Empleado empleado= new Empleado();
+                Empleado empleado = new Empleado();
                 empleado.setNss(resultado.getInt("NSS"));
                 empleado.setNom(resultado.getString("NOM"));
                 empleado.setApellidos(resultado.getString("APELLIDOS"));
@@ -40,30 +49,38 @@ public class EmpleadoDao implements Dao<Empleado, Integer> {
                 return empleado;
             }
         } catch (SQLException e) {
-            System.err.println("Err Recojer Empleado: "+e.getMessage());
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err Recojer Empleado: " + e.getMessage());
         }
+        // en caso de no haber nadie con ese id, no devuelve nada
         return null;
     }
 
     @Override
     public List<Empleado> listarTodos() {
+        // comando que se usara para listar todos los empleados
         String comando = "SELECT * FROM EMPLEADO";
+        // arrayList para poder guardar todos los empleados listados
         List<Empleado> listaEmp = new ArrayList<>();
 
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
         try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
                 ResultSet resultado = statement.executeQuery()) {
             
+            //bucle que listara al empleado segun la cantidad de resultados del comando
             while (resultado.next()) {
-                Empleado empleado= new Empleado();
+                Empleado empleado = new Empleado();
                 empleado.setNss(resultado.getInt("NSS"));
                 empleado.setNom(resultado.getString("NOM"));
                 empleado.setApellidos(resultado.getString("APELLIDOS"));
                 empleado.setEmail(resultado.getString("EMAIL"));
                 empleado.setIban(resultado.getString("IBAN"));
-                listaEmp.add(empleado);
+                listaEmp.add(empleado);//se añade a la lista
             }
         } catch (SQLException e) {
-            System.err.println("Err listar Empleado: "+e.getMessage());
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err listar Empleado: " + e.getMessage());
         }
 
         return listaEmp;
@@ -71,9 +88,12 @@ public class EmpleadoDao implements Dao<Empleado, Integer> {
 
     @Override
     public void editar(Empleado empleado) {
-        String comando= "UPDATE EMPLEADO SET NSS=?, NOM=?, APELLIDO=?, EMAIL=?, IBAN=? WHERE ID=?";
+        //este es el comando que se utilizara para hacer update al empleado
+        String comando = "UPDATE EMPLEADO SET NSS=?, NOM=?, APELLIDO=?, EMAIL=?, IBAN=? WHERE ID=?";
 
-        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando)){
+        //aqui se prepara la base de datos junto a su conexion y le pasamos el comando
+        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando)) {
+            // por cada interrogante se sustituira por el valor en orden del empleado
             statement.setInt(1, empleado.getNss());
             statement.setString(2, empleado.getNom());
             statement.setString(3, empleado.getApellidos());
@@ -83,20 +103,25 @@ public class EmpleadoDao implements Dao<Empleado, Integer> {
 
             statement.executeUpdate();
         } catch (SQLException e) {
-            System.err.println("Err editar empleado: "+e.getMessage());
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err editar empleado: " + e.getMessage());
         }
     }
 
     @Override
     public void eliminarPorId(Integer id) {
+        //comando para eliminar empleados por id
         String comando = "DELETE * FROM EMPLEADO WHERE ID=?";
-
+        
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
         try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
                 ResultSet resultado = statement.executeQuery()) {
             statement.setInt(1, id);
-
+            
+            // este bucle elimina al empleado que contenga el numero de id
             while (resultado.next()) {
-                Empleado empleado= new Empleado();
+                Empleado empleado = new Empleado();
                 empleado.setNss(resultado.getInt("NSS"));
                 empleado.setNom(resultado.getString("NOM"));
                 empleado.setApellidos(resultado.getString("APELLIDOS"));
@@ -104,7 +129,8 @@ public class EmpleadoDao implements Dao<Empleado, Integer> {
                 empleado.setIban(resultado.getString("IBAN"));
             }
         } catch (SQLException e) {
-            System.err.println("Err Recojer Empleado: "+e.getMessage());
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err Recojer Empleado: " + e.getMessage());
         }
     }
 }
--- Entregables/prou8ex01-fran/src/main/java/es/programacion/cide/DAO/NominaDao.java
+++ Entregables/prou8ex01-fran/src/main/java/es/programacion/cide/DAO/NominaDao.java
@@ -1,44 +1,135 @@
 package es.programacion.cide.DAO;
+
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.programacion.cide.db.DataBaseManager;
+import es.programacion.cide.model.Empleado;
 import es.programacion.cide.model.Nomina;
 
 public class NominaDao implements Dao<Nomina, Integer> {
 
     @Override
     public void insertar(Nomina nomina) {
-        String comando = "INSERT INTO nomina (NSS,NOM,APELLIDOS,EMAIL,IBAN) VALUES(?,?,?,?,?)";
+        // este es el comando en sqlite para insertar valores a la base de datos
+        String comando = "INSERT INTO NOMINA (IBAN_PAGAMENT,IMPORT_REAL,NSS_EMPLEADO,CODI_PLAZA) VALUES(?,?,?,?)";
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando
         try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando)) {
-            statement.setInt(1, nomina.getNss());
-            statement.setString(2, nomina.getNom());
-            statement.setString(3, nomina.getApellidos());
-            statement.setString(4, nomina.getEmail());
-            statement.setString(5, nomina.getIban());
+            // por cada interrogante dentro de values recojemos los datos de la Nomina y se
+            // pone en values
+            statement.setString(1, nomina.getIbanPag());
+            statement.setString(2, nomina.getImporte());
+            statement.setInt(3, nomina.getNssEmpleado());
+            statement.setInt(4, nomina.getCodiPlaza());
         } catch (SQLException e) {
-            System.err.println("Err insert empelado: " + e.getMessage());
+            // en caso de error al preparar la conexion, se imprime el mensaje del error
+            System.err.println("Err insert Nomina: " + e.getMessage());
         }
     }
 
     @Override
     public Nomina recojerPorId(Integer id) {
+        // este es el comando que servira para recojer una Nomina por id
+        String comando = "SELECT * FROM NOMINA WHERE ID=?";
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
+        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
+                ResultSet resultado = statement.executeQuery()) {
+            // se le pasa el valor del interogante, con el id que se busca
+            statement.setInt(1, id);
+
+            // este bucle lista las Nominas que contenga el numero de id
+            while (resultado.next()) {
+                Nomina nomina = new Nomina();
+                nomina.setIbanPag(resultado.getString("IBAN_PAGAMENT"));
+                nomina.setImporte(resultado.getString("IMPORT_REAL"));
+                nomina.setNssEmleado(resultado.getInt("NSS_EMPLEADO"));
+                nomina.setCodiPlaza(resultado.getInt("CODI_PLAZA"));
+                return nomina;
+            }
+        } catch (SQLException e) {
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err Recojer Nomina: " + e.getMessage());
+        }
+        // en caso de no haber ninguna nomina con ese id, no devuelve nada
         return null;
     }
 
     @Override
     public List<Nomina> listarTodos() {
-        return null;
+        // comando que se usara para listar todas las nominas
+        String comando = "SELECT * FROM NOMINA";
+        // arrayList para poder guardar todas las nominas listadas
+        List<Nomina> listaNom = new ArrayList<>();
+
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
+        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
+                ResultSet resultado = statement.executeQuery()) {
+            
+            //bucle que listara la nomina segun cuantos resultados de el comando
+            while (resultado.next()) {
+                Nomina nomina = new Nomina();
+                nomina.setIbanPag(resultado.getString("IBAN_PAGAMENT"));
+                nomina.setImporte(resultado.getString("IMPORT_REAL"));
+                nomina.setNssEmleado(resultado.getInt("NSS_EMPLEADO"));
+                nomina.setCodiPlaza(resultado.getInt("CODI_PLAZA"));
+                listaNom.add(nomina);//se añade a la lista
+            }
+        } catch (SQLException e) {
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err listar NOMINA: " + e.getMessage());
+        }
+
+        return listaNom;
     }
 
     @Override
-    public void editar(Nomina valor) {
-            
+    public void editar(Nomina nomina) {
+        //este es el comando que se utilizara para hacer update a la nomina
+        String comando = "UPDATE NOMINA SET IBAN_PAGAMENT=?, IMPORT_REAL=?, NSS_EMPLEADO=?, CODI_PLAZA=? WHERE ID=?";
+
+        //aqui se prepara la base de datos junto a su conexion y le pasamos el comando
+        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando)) {
+            // por cada interrogante se sustituira por el valor en orden de la nomina
+            statement.setString(1, nomina.getIbanPag());
+            statement.setString(2, nomina.getImporte());
+            statement.setInt(3, nomina.getNssEmpleado());
+            statement.setInt(4, nomina.getCodiPlaza());
+            statement.setInt(5, nomina.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err editar NOMINA: " + e.getMessage());
+        }
     }
 
     @Override
     public void eliminarPorId(Integer id) {
+        //comando para eliminar nominas por id
+        String comando = "DELETE * FROM NOMINA WHERE ID=?";
+        
+        // aqui se prepara la base de datos junto a su conexion y le pasamos el comando,
+        // ademas usamos resulset para ejecutar el comando
+        try (PreparedStatement statement = DataBaseManager.getConnection().prepareStatement(comando);
+                ResultSet resultado = statement.executeQuery()) {
+            statement.setInt(1, id);
             
+            // este bucle elimina las nominas que contenga el numero de id
+            while (resultado.next()) {
+                Nomina nomina = new Nomina();
+                nomina.setIbanPag(resultado.getString("IBAN_PAGAMENT"));
+                nomina.setImporte(resultado.getString("IMPORT_REAL"));
+                nomina.setNssEmleado(resultado.getInt("NSS_EMPLEADO"));
+                nomina.setCodiPlaza(resultado.getInt("CODI_PLAZA"));
+            }
+        } catch (SQLException e) {
+            // si el try devuelve error, se imprimira el mensaje
+            System.err.println("Err Recojer NOMINA: " + e.getMessage());
+        }
     }
 }
